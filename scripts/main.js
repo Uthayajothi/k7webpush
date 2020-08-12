59df0d0e37db67749e9a493357796f860aca9412
Uthayajothi <69522308+Uthayajothi@users.noreply.github.com>
2020-08-12 11:40:10 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -139,7 +139,7 @@ function updateSubscriptionOnServer(subscription) {
   if (subscription) {
     subscriptionJson.textContent = JSON.stringify(subscription);
 	  
-	  //MktoForms2.loadForm("//app-sn02.marketo.com", "552-EZZ-853","1067");
+	  MktoForms2.loadForm("//app-sn02.marketo.com", "552-EZZ-853","1067");
 		  MktoForms2.whenReady(function(form){
 		  $("#form1067").submit(function(){    
 		var myForm = MktoForms2.allForms()[0];
